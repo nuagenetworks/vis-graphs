24707feffba08f837b845f7c76f66c9b3c7f21fa
mayank-nx <mayank@nodexperts.com>
2018-09-19 12:44:03 +0530

    feedback changes in table

--- Graphs/Table/tableGraph.test.js
+++ Graphs/Table/tableGraph.test.js
@@ -1,170 +1,211 @@
 import React from 'react';
-import {mount} from 'enzyme';
-import {CheckTicks,getDataAndConfig} from '../testHelper';
+import { mount } from 'enzyme';
+
+import { getDataAndConfig } from '../testHelper';
 import Table from '.';
+
 const cheerio = require('cheerio')
 
-describe ("test Table", ()=>{
-    beforeAll( async () => {
-            global.config = await getDataAndConfig('Table');    
+describe("Table", () => {
+    let config;
+    beforeAll(async () => {
+        config = await getDataAndConfig('Table');
     });
 
-    describe("hide-pagination", ()=>{
-        beforeAll( async () => {
-            global.componentHidePagination = mount(<Table width={500} height={500} configuration={config.hidePagination} data={config.data}> </Table>);
+    describe("hidePagination", () => {
+        let hidePagination, table, $;
+        beforeAll(async () => {
+            hidePagination = mount(
+                <Table
+                    width={500}
+                    height={500}
+                    configuration={config.hidePagination}
+                    data={config.data}>
+                </Table>
+            );
+            table = hidePagination.find('table').html();
+            $ = cheerio.load(table);
         });
 
-        it("number of column Table is matched", ()=>{
-            const middleHtml = componentHidePagination.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const noOfBars = $('table').find('thead').find('tr').children().length;
-            const totalBar = componentHidePagination.root.props().configuration.data.columns.length;
-            expect(noOfBars).toBe(totalBar)
+        it("Number of column", () => {
+            const noOfColumns = $('table').find('thead').find('tr').children().length;
+            const totalCoulmn = hidePagination.root.props().configuration.data.columns.length;
+            expect(noOfColumns).toBe(totalCoulmn);
         });
 
-        it("check-column-names", ()=>{
-            var obj = componentHidePagination.root.props().configuration.data.columns;
-            var columns = Object.keys(obj).map(function(key) {
-            return obj[key].column;
+        it("Check column names", () => {
+            var columnList = hidePagination.root.props().configuration.data.columns;
+            var columns = Object.keys(columnList).map(function (key) {
+                return columnList[key].column;
             });
-            const middleHtml = componentHidePagination.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const tableColumns = $('table thead th').map(function(i) { return $(this).text(); }).get();
+            const tableColumns = $('table thead th').map(
+                function (i) {
+                    return $(this).text();
+                }
+            ).get();
             expect(tableColumns).toEqual(columns);
         });
-    
-        it("check-row", ()=>{
-            const middleHtml = componentHidePagination.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const value = $('table tbody tr').first().find('td').map(function(i) { return $(this).text(); }).get();
+
+        it("Check row", () => {
+            const value = $('table tbody tr').first().find('td').map(
+                function (i) {
+                    return $(this).text();
+                }
+            ).get();
             expect(value).toBeDefined();
         });
 
-        it("check-number-of-rows", ()=>{
-            const middleHtml = componentHidePagination.find('table').html();
-            const $ = cheerio.load(middleHtml);
+        it("Number of rows", () => {
             const noOfRows = $('table tbody tr').length;
-            expect(noOfRows).toBeGreaterThan(0);
-        })     
-    });   
+            expect(noOfRows).toBe(3);
+        })
+    });
 
-    describe("hide-search-bar", ()=>{
-        beforeAll( async () => {
-            global.componentHideSearch = mount(<Table width={500} height={500} configuration={config.hideSearchBar} data={config.data}> </Table>);
+    describe("hideSearchBar", () => {
+        let hideSearchBar, table, $;
+        beforeAll(async () => {
+            hideSearchBar = mount(
+                <Table
+                    width={500}
+                    height={500}
+                    configuration={config.hideSearchBar}
+                    data={config.data}>
+                </Table>
+            );
+            table = hideSearchBar.find('table').html();
+            $ = cheerio.load(table);
         });
 
-        it("number of column Table is matched", ()=>{
-            const middleHtml = componentHideSearch.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const noOfBars = $('table').find('thead').find('tr').children().length;
-            const totalBar = componentHideSearch.root.props().configuration.data.columns.length;
-            expect(noOfBars).toBe(totalBar)
+        it("Number of column", () => {
+            const noOfColumns = $('table').find('thead').find('tr').children().length;
+            const totalCoulmn = hideSearchBar.root.props().configuration.data.columns.length;
+            expect(noOfColumns).toBe(totalCoulmn);
         });
 
-        it("check-column-names", ()=>{
-            var obj = componentHideSearch.root.props().configuration.data.columns;
-            var columns = Object.keys(obj).map(function(key) {
-            return obj[key].column;
+        it("Check column names", () => {
+            var columnList = hideSearchBar.root.props().configuration.data.columns;
+            var columns = Object.keys(columnList).map(function (key) {
+                return columnList[key].column;
             });
-            const middleHtml = componentHideSearch.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const tableColumns = $('table thead th').map(function(i) { return $(this).text(); }).get();
+            const tableColumns = $('table thead th').map(
+                function (i) {
+                    return $(this).text();
+                }
+            ).get();
             expect(tableColumns).toEqual(columns);
         });
-    
-        it("check-row", ()=>{
-            const middleHtml = componentHideSearch.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const value = $('table tbody tr').first().find('td').map(function(i) { return $(this).text(); }).get();
+
+        it("Check row", () => {
+            const value = $('table tbody tr').first().find('td').map(
+                function (i) {
+                    return $(this).text();
+                }
+            ).get();
             expect(value).toBeDefined();
         });
 
-        it("check-number-of-rows", ()=>{
-            const middleHtml = componentHideSearch.find('table').html();
-            const $ = cheerio.load(middleHtml);
+        it("Number of rows", () => {
             const noOfRows = $('table tbody tr').length;
-            expect(noOfRows).toBeGreaterThan(0);
-        })   
-    });    
+            expect(noOfRows).toBe(3);
+        })
+    });
 
-    let componentselectAll;
-    describe("selectAll", ()=>{
-        beforeAll( async () => {
-           componentselectAll = mount(<Table width={500} height={500} configuration={config.selectAll} data={config.data}> </Table>);
+    describe("selectAll", () => {
+        let selectAll, table, $;
+        beforeAll(async () => {
+            selectAll = mount(
+                <Table
+                    width={500}
+                    height={500}
+                    configuration={config.selectAll}
+                    data={config.data}>
+                </Table>
+            );
+            table = selectAll.find('table').html();
+            $ = cheerio.load(table);
         });
 
-        it("number of column Table is matched", ()=>{
-            const middleHtml = componentselectAll.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const noOfBars = $('table').find('thead').find('tr').children().length;
-            const totalBar = componentselectAll.root.props().configuration.data.columns.length;
-            expect(noOfBars).toBe(totalBar+1)
+        it("Number of column", () => {
+            const noOfColumns = $('table').find('thead').find('tr').children().length;
+            const totalCoulmn = selectAll.root.props().configuration.data.columns.length;
+            expect(noOfColumns).toBe(totalCoulmn + 1);
         });
 
-        it("check-column-names", ()=>{
-            var obj = componentselectAll.root.props().configuration.data.columns;
-            var columns = Object.keys(obj).map(function(key) {
-                return obj[key].column;
+        it("Check column names", () => {
+            var columnList = selectAll.root.props().configuration.data.columns;
+            var columns = Object.keys(columnList).map(function (key) {
+                return columnList[key].column;
             });
             columns.unshift("");
-            const middleHtml = componentselectAll.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const tableColumns = $('table thead th').map(function(i) { return $(this).text(); }).get();
+            const tableColumns = $('table thead th').map(
+                function (i) {
+                    return $(this).text();
+                }
+            ).get();
             expect(tableColumns).toEqual(columns);
         });
-    
-        it("check-row", ()=>{
-            const middleHtml = componentselectAll.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const value = $('table tbody tr').first().find('td').map(function(i) { return $(this).text(); }).get();
+
+        it("Check row", () => {
+            const value = $('table tbody tr').first().find('td').map(
+                function (i) {
+                    return $(this).text();
+                }
+            ).get();
             expect(value).toBeDefined();
         });
 
-        it("check-number-of-rows", ()=>{
-            const middleHtml = componentselectAll.find('table').html();
-            const $ = cheerio.load(middleHtml);
+        it("Number of rows", () => {
             const noOfRows = $('table tbody tr').length;
-            expect(noOfRows).toBeGreaterThan(0);
-        })   
-    }); 
-
-    let componentSimple;
-    describe("highlight-column", ()=>{
-        beforeAll( async () => {
-            componentSimple = mount(<Table width={500} height={500} configuration={config.highlightColumn} data={config.data}> </Table>);
-        });
-
-        it("number of column Table is matched", ()=>{
-            const middleHtml = componentSimple.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const noOfBars = $('table').find('thead').find('tr').children().length;
-            const totalBar = componentSimple.root.props().configuration.data.columns.length;
-            expect(noOfBars).toBe(totalBar)
-        });
-
-        it("check-column-names", ()=>{
-            var obj = componentSimple.root.props().configuration.data.columns;
-            var columns = Object.keys(obj).map(function(key) {
-            return obj[key].column;
-            });
-            const middleHtml = componentSimple.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const tableColumns = $('table thead th').map(function(i) { return $(this).text(); }).get();
-            expect(tableColumns).toEqual(columns);
-        });
-    
-        it("check-row", ()=>{
-            const middleHtml = componentSimple.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const value = $('table tbody tr').first().find('td').map(function(i) { return $(this).text(); }).get();
-            expect(value).toBeDefined();
-        });
-
-        it("check-number-of-rows", ()=>{
-            const middleHtml = componentSimple.find('table').html();
-            const $ = cheerio.load(middleHtml);
-            const noOfRows = $('table tbody tr').length;
-            expect(noOfRows).toBeGreaterThan(0);
+            expect(noOfRows).toBe(3);
         })
-    }); 
+    });
+
+    describe("highlightColumn", () => {
+        let highlightColumn, table, $;
+        beforeAll(async () => {
+            highlightColumn = mount(
+                <Table
+                    width={500}
+                    height={500}
+                    configuration={config.highlightColumn}
+                    data={config.data}>
+                </Table>
+            );
+            table = highlightColumn.find('table').html();
+            $ = cheerio.load(table);
+        });
+
+        it("Number of column", () => {
+            const noOfColumns = $('table').find('thead').find('tr').children().length;
+            const totalCoulmn = highlightColumn.root.props().configuration.data.columns.length;
+            expect(noOfColumns).toBe(totalCoulmn);
+        });
+
+        it("Check column names", () => {
+            var columnList = highlightColumn.root.props().configuration.data.columns;
+            var columns = Object.keys(columnList).map(function (key) {
+                return columnList[key].column;
+            });
+            const tableColumns = $('table thead th').map(
+                function (i) {
+                    return $(this).text();
+                }
+            ).get();
+            expect(tableColumns).toEqual(columns);
+        });
+
+        it("Check row", () => {
+            const value = $('table tbody tr').first().find('td').map(
+                function (i) {
+                    return $(this).text();
+                }
+            ).get();
+            expect(value).toBeDefined();
+        });
+
+        it("number of rows", () => {
+            const noOfRows = $('table tbody tr').length;
+            expect(noOfRows).toBe(3);
+        })
+    });
 });
