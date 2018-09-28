f1da021171b01caa0a2022980d7e5c12c27a041b
mayank-nx <mayank@nodexperts.com>
2018-09-28 11:41:39 +0530

    straight line test case

--- Graphs/LineGraph/lineGraph.test.js
+++ Graphs/LineGraph/lineGraph.test.js
@@ -60,7 +60,7 @@ describe("LineGraph", () => {
 
     });
 
-    describe("simple-lineGraph", () => {
+    describe("Simple LineGraph", () => {
         let simpleLine, $;
         beforeAll(async () => {
             simpleLine = mount(
@@ -104,6 +104,132 @@ describe("LineGraph", () => {
         it("Legends", () => {
             const legend = $('svg').find('g').get(1).nextSibling.children.length;
             expect(legend).toBe(3);
+        });
+    });
+
+    describe("simple LineGraph With Ycolumn ", () => {
+        let ycolumn, $;
+        beforeAll(async () => {
+            ycolumn = mount(
+                <LineGraph
+                    width={500}
+                    height={500}
+                    configuration={config.simpleYcolumn}
+                    data={config.data}>
+                </LineGraph>
+            );
+            $ = getHtml(ycolumn, 'svg');;
+        });
+
+        it("SVG Dimensions", () => {
+            const result = checkSvg(ycolumn);
+            expect(result).toBeTruthy();
+        });
+
+        it("Total LineGraph", () => {
+            const noOfLines = $('svg').find('g').get(1).firstChild.nextSibling.nextSibling.children.length;
+            expect(noOfLines).toBe(1)
+        });
+
+        it("xAxis Ticks Length", () => {
+            const xAxisTicks = $('svg').find('g').get(1).firstChild.children.length;
+            expect(xAxisTicks).toBe(11);
+        });
+
+        it("yAxis Ticks Length", () => {
+            const yAxisTicks = $('svg').find('g').get(1).firstChild.nextSibling.children.length;
+            expect(yAxisTicks).toBe(5);
+        });
+
+        it("Check Line", () => {
+            const linePath = $('svg').find('g').get(1).firstChild.nextSibling.nextSibling.children;
+            const line = cheerio.load(linePath);
+            const d = line('path').attr('d');
+            expect(d).toEqual("M0,445.9741367817624L149.03225806451613,436.93534195440606L284.51612903225805,442.96120517264364L420,412.8318890814558");
+        });
+
+        it("Legends", () => {
+            const legend = $('svg').find('g').get(1).nextSibling.children.length;
+            expect(legend).toBe(1);
+        });
+
+        it("Straight Line", () => {
+            const straightLine = $('svg').
+                find('g')
+                .get(1)
+                .firstChild
+                .nextSibling
+                .nextSibling
+                .nextSibling
+                .nextSibling
+                .nextSibling
+                .nextSibling
+                .children
+                .length;
+            expect(straightLine).toBe(2);
+        });
+    });
+
+    describe("Multiline LineGraph With Ycolumn ", () => {
+        let multilineYcolumn, $;
+        beforeAll(async () => {
+            multilineYcolumn = mount(
+                <LineGraph
+                    width={500}
+                    height={500}
+                    configuration={config.multilineYcolumn}
+                    data={config.data}>
+                </LineGraph>
+            );
+            $ = getHtml(multilineYcolumn, 'svg');;
+        });
+
+        it("SVG Dimensions", () => {
+            const result = checkSvg(multilineYcolumn);
+            expect(result).toBeTruthy();
+        });
+
+        it("Total LineGraph", () => {
+            const noOfLines = $('svg').find('g').get(1).firstChild.nextSibling.nextSibling.children.length;
+            expect(noOfLines).toBe(3)
+        });
+
+        it("xAxis Ticks Length", () => {
+            const xAxisTicks = $('svg').find('g').get(1).firstChild.children.length;
+            expect(xAxisTicks).toBe(11);
+        });
+
+        it("yAxis Ticks Length", () => {
+            const yAxisTicks = $('svg').find('g').get(1).firstChild.nextSibling.children.length;
+            expect(yAxisTicks).toBe(6);
+        });
+
+        it("Check Line", () => {
+            const linePath = $('svg').find('g').get(1).firstChild.nextSibling.nextSibling.children;
+            const line = cheerio.load(linePath);
+            const d = line('path').attr('d');
+            expect(d).toEqual("M0,400.6363636363636L142.6451612903226,323.5909090909091L272.3225806451613,374.95454545454544L402,118.13636363636363");
+        });
+
+        it("Legends", () => {
+            const legend = $('svg').find('g').get(1).nextSibling.children.length;
+            expect(legend).toBe(3);
+        });
+
+        it("Straight Line", () => {
+            const straightLine = $('svg').
+                find('g')
+                .get(1)
+                .firstChild
+                .nextSibling
+                .nextSibling
+                .nextSibling
+                .nextSibling
+                .nextSibling
+                .nextSibling
+                .children
+                .length;
+            expect(straightLine).toBe(2);
         });
     });
 });
