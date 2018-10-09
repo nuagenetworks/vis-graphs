0639d29bdc8b4261c9f3fec50261ba8fb7018365
mayank-nx <mayank@nodexperts.com>
2018-10-09 19:02:32 +0530

    feedback changes

--- Graphs/ChordGraph/chordGraph.test.js
+++ Graphs/ChordGraph/chordGraph.test.js
@@ -30,17 +30,17 @@ describe("ChordGraph", () => {
 
         it("Total ChordGraph", () => {
             const noOGraphs = $('svg').find('g').length;
-            expect(noOGraphs).toBe(11)
+            expect(noOGraphs).toBe(12)
         });
 
         it("ChordGraph Path Definition", () => {
             const chordGraphDefinition = $('svg').find('g').find('g').find('path').attr("d");
-            expect(chordGraphDefinition).toEqual("M1.2246467991473532e-14,-200A200,200,0,0,1,131.1836030421305,-150.96642770127647Q0,0,141.4213562373095,-141.42135623730948A200,200,0,0,1,199.51020005065592,-13.988569467506565Q0,0,1.2246467991473532e-14,-200Z");
+            expect(chordGraphDefinition).toEqual("M1.2246467991473532e-14,-200A200,200,0,0,1,107.13696325155514,-168.88360223904192Q0,0,118.68678486490563,-160.97654207502288A200,200,0,0,1,186.45394819404802,-72.35278296548967Q0,0,1.2246467991473532e-14,-200Z");
         });
 
         it("ChordGraph Transform", () => {
             const chordGraphTransform = $('svg').find('g').find('g').find('text').attr("transform");
-            expect(chordGraphTransform).toEqual("rotate(-69.50535228295789)translate(230)");
+            expect(chordGraphTransform).toEqual("rotate(-73.8048170546621)translate(230)");
         });
     });
 });
