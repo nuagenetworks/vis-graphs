5e0d6324f3d5d5c9470e977b3ec986c62920d134
Bharat Mukheja <bharat.mukheja@gmail.com>
2018-10-10 16:20:54 -0700

    Fixed a bug

--- Graphs/GeoMap/index.js
+++ Graphs/GeoMap/index.js
@@ -348,7 +348,7 @@ class GeoMap extends AbstractGraph {
 
     this.timerId = setTimeout(
       () => this.setClusterIcons(clusters),
-      100
+      0
     );
 
     if (!_.isEqual(this.markers, markers)) {
