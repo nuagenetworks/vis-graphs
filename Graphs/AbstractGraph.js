b3e15578c6c586603200d177b8a85a27a39f1dc5
nxjiten <jiten@nodexperts.com>
2019-07-05 14:22:32 +0530

    reduce delay time

--- Graphs/AbstractGraph.js
+++ Graphs/AbstractGraph.js
@@ -69,7 +69,7 @@ export default class AbstractGraph extends React.Component {
                     getContent={[() => this.getTooltipContent(this.hoveredDatum), 200]}
                     afterHide={() => this.handleHideEvent()}
                     afterShow={() => this.handleShowEvent()}
-                    delayUpdate={500}
+                    delayUpdate={200}
                 />
             );
 
