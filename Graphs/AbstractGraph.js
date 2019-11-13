d3928949148f68631ad21b770aa529861e2defec
Natalia Balus <nataba@gmail.com>
2019-11-12 16:21:37 -0800

    Check for array.

--- Graphs/AbstractGraph.js
+++ Graphs/AbstractGraph.js
@@ -125,6 +125,10 @@ export default class AbstractGraph extends React.Component {
             yTicksLabel = {};
         }
 
+        if (!Array.isArray(tooltip)) {
+            return null;
+        }
+
         return (
             /* Display each tooltip column as "label : value". */
             tooltip.map(({ column, label }, i) => {
