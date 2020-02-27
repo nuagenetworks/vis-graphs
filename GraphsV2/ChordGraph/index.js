b55e410c4c6579b8f0005b9c9b4b6f3e8f43337b
nxanil <anil@nodexperts.com>
2020-02-27 16:09:57 +0530

    Feedback changes

--- GraphsV2/ChordGraph/index.js
+++ GraphsV2/ChordGraph/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ReactTooltip from "react-tooltip";
 import PropTypes from 'prop-types';
 import * as d3 from 'd3';
 import { compose } from 'redux';
@@ -14,11 +15,127 @@ import Ribbons from './Ribbons';
 import { config } from './default.config';
 import WithConfigHOC from '../../HOC/WithConfigHOC';
 import WithValidationHOC from '../../HOC/WithValidationHOC';
-import customTooltip from '../utils/customTooltip';
+import columnAccessor from "../../utils/columnAccessor";
 
 const Tooltip = styled('div')({});
 
 const Container = styled('div')({});
+
+const tooltipWrapper = (data, id, tooltip) => (<ReactTooltip
+    id={id}
+    place="top"
+    type="dark"
+    effect="float"
+    getContent={[() => getTooltipContent(data, tooltip), 200]}
+    delayUpdate={200}
+/>)
+
+const getTooltipContent=(data, tooltip) => {
+    if (data && data.destination) {
+        const { accessor, label } = (
+            (tooltip && tooltip.length === 1)
+                ? { accessor: columnAccessor(tooltip[0]), label: tooltip[0].label }
+                : { accessor: (d) => d.value, label: undefined }
+        );
+        return chordTooltipContent(data, accessor, label)
+    } else {
+        return <div>Hover over a chord to see flow details.</div>;
+    }
+}
+
+const chordTooltipContent = (data, accessor, label) => {
+    const {
+        source,
+        destination,
+        sourceValue,
+        destinationValue,
+        isBidirectionalTooltip
+    } = data;
+
+    return (
+        <React.Fragment>
+            {renderTooltipContent(destination, source, sourceValue, accessor, label)}
+            {isBidirectionalTooltip && renderTooltipContent(source, destination, destinationValue, accessor, label)}
+        </React.Fragment>
+    );
+}
+
+const renderTooltipContent = (from, to, value, accessor, label) => (
+    <div>
+        <strong>{`${from} to ${to}:`}</strong>
+        <span> {accessor({ value })}</span>
+        {label ? <span> {label}</span> : null}
+    </div>
+);
+
+const weight = (d, chordWeightColumn) => d[chordWeightColumn];
+const destination = (d, chordDestinationColumn) => d[chordDestinationColumn];
+const source = (d, chordSourceColumn) => d[chordSourceColumn]; 
+
+const generateMatrix = ({
+    data, 
+    chordWeightColumn, 
+    chordSourceColumn,
+    chordDestinationColumn
+}) => {
+    if (isEmpty(data)) {
+        return []
+    }
+
+    let indices = {},
+        matrix = [],
+        names = [],
+        matrixData = [],
+        n = 0, i, j;
+
+    const recordIndex = (name) =>{
+        if (!(name in indices)) {
+            indices[name] = n++;
+            names.push(name);
+        }
+    }
+
+    data.forEach(d => {
+        recordIndex(source(d, chordSourceColumn));
+        recordIndex(destination(d, chordDestinationColumn));
+    });
+
+    for (i = 0; i < n; i++) {
+        matrix.push([]);
+        matrixData.push([]);
+        for (j = 0; j < n; j++) {
+            matrix[i].push(0);
+            matrixData[i].push([]);
+        }
+    }
+
+    data.forEach(d => {
+        i = indices[source(d, chordSourceColumn)];
+        j = indices[destination(d, chordDestinationColumn)];
+
+        if (chordWeightColumn) {
+            matrix[j][i] += weight(d, chordWeightColumn);
+        } else {
+
+            // Handle the case where no weight column was specified
+            // by making the chord weight fixed on both sides.
+            matrix[j][i] = matrix[i][j] = 1;
+        }
+        matrixData[j][i].push(d);
+    });
+
+    matrix.names = names;
+    matrix.data = matrixData;
+
+    return matrix;
+}
+
+const getFilterData = (data, chordSourceColumn, chordDestinationColumn) => {
+    return data.filter(d => d[chordSourceColumn] && d[chordDestinationColumn]);
+}
+
+let filterChordData = [];
+let matrix = [];
 
 const ChordGraph = (props) => {
 
@@ -26,7 +143,6 @@ const ChordGraph = (props) => {
     const [stateMouseOverGroup, setStateMouseOverGroup] = useState(null);
     const [stateMouseOverRibbon, setStateMouseOverRibbon] = useState(null);
     const [filterData, setFilterData] = useState([]);
-    const [customTooltips, setCustomTooltips] = useState({});
     const [hoverData, setHoverData] = useState({ isChord: true });
 
     const {
@@ -57,19 +173,24 @@ const ChordGraph = (props) => {
         colors,
         additionalKeys,
         bidirectionalTooltip,
-        id
+        id,
+        tooltip,
     } = properties
 
     useEffect(() => {
-        setCustomTooltips(customTooltip(properties));
         parseData(props.data);
     }, [props.data, props.width, props.height]);
 
     const parseData = (data) => {
-
-        const filterChordData = data.filter(d => d[chordSourceColumn] && d[chordDestinationColumn])
-
+        filterChordData = getFilterData(data, chordSourceColumn, chordDestinationColumn);
+        
         if (!isEqual(filterChordData, filterData)) {
+            matrix = generateMatrix({
+                data: filterChordData, 
+                chordWeightColumn, 
+                chordSourceColumn,
+                chordDestinationColumn
+            });
             setFilterData(filterChordData);
         }
     }
@@ -86,71 +207,9 @@ const ChordGraph = (props) => {
 
     const setMouseOverGroup = (mouseOverGroup) => {
         setStateMouseOverGroup(mouseOverGroup);
-    }
+    }    
 
-    const weight = function (d) { return d[chordWeightColumn]; };
-    const source = function (d) { return d[chordSourceColumn]; };
-    const destination = function (d) { return d[chordDestinationColumn]; };
-    // Generates a matrix (2D array) from the given data, which is expected to
-    // have fields {origin, destination, count}. The matrix data structure is required
-    // for use with the D3 Chord layout.
-
-    const generateMatrix = (data) => {
-        if (isEmpty(data)) {
-            return []
-        }
-
-        var indices = {},
-            matrix = [],
-            names = [],
-            matrixData = [],
-            n = 0, i, j;
-
-        function recordIndex(name) {
-            if (!(name in indices)) {
-                indices[name] = n++;
-                names.push(name);
-            }
-        }
-
-        data.forEach(function (d) {
-            recordIndex(source(d));
-            recordIndex(destination(d));
-        });
-
-        for (i = 0; i < n; i++) {
-            matrix.push([]);
-            matrixData.push([]);
-            for (j = 0; j < n; j++) {
-                matrix[i].push(0);
-                matrixData[i].push([]);
-            }
-        }
-
-        data.forEach(function (d) {
-            i = indices[source(d)];
-            j = indices[destination(d)];
-
-            if (chordWeightColumn) {
-                matrix[j][i] += weight(d);
-            } else {
-
-                // Handle the case where no weight column was specified
-                // by making the chord weight fixed on both sides.
-                matrix[j][i] = matrix[i][j] = 1;
-            }
-            matrixData[j][i].push(d);
-        });
-
-        matrix.names = names;
-        matrix.data = matrixData;
-
-        return matrix;
-    }
-
-    const matrix = generateMatrix(filterData);
     const labels = !isEmpty(matrix) ? matrix.names : [];
-
     const outerRadius = props.outerRadius || Math.min(width, height) * 0.4 - 40;
     const innerRadius = props.innerRadius || outerRadius - 30;
 
@@ -207,7 +266,7 @@ const ChordGraph = (props) => {
 
     return (
         <Container data-test='chord-graph'>
-            <Tooltip> {customTooltips.tooltipWrapper && customTooltips.tooltipWrapper(hoverData)} </Tooltip>
+            <Tooltip> {tooltipWrapper(hoverData, id, tooltip)} </Tooltip>
             <Svg
                 width={width}
                 height={height}
