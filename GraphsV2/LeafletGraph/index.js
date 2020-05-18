aa481ab5b02e4f136cdeb6f8dc99785414df4535
nxanil <anil@nodexperts.com>
2020-05-18 15:44:30 +0530

    Add polyline between two markers

--- GraphsV2/LeafletGraph/index.js
+++ GraphsV2/LeafletGraph/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
-import { Map, TileLayer, Marker, Tooltip } from "react-leaflet";
+import { Map, TileLayer, Marker, Tooltip, Polyline } from "react-leaflet";
 import isEqual from 'lodash/isEqual';
 import MarkerClusterer from "react-leaflet-markercluster";
 import { compose } from 'redux';
@@ -14,6 +14,7 @@ import { getIconPath } from '../../utils/helpers';
 import { config } from './default.config';
 import { styled } from '@material-ui/core/styles';
 import './styles.css';
+import { theme } from '../../theme';
 
 const InfoContainer = styled('div')({
   display: 'table'
@@ -45,6 +46,7 @@ const handleMarkerClick = (data, onMarkClick) => {
 const LeafletGraph = (props) => {
 
   const [stateData, setStateData] = useState([]);
+  const [line, setLine] = useState([]);
   const [infowindow, setInfowindow] = useState({ data: null, position: null });
 
   useEffect(() => {
@@ -72,11 +74,12 @@ const LeafletGraph = (props) => {
     markerIcon,
     filters,
     tooltip,
+    links,
   } = properties;
 
   const initiate = (props) => {
-    //markers = new Map();
     setStateData(props.data);
+    setLine([]);
     setInfowindow({ data: null, position: null });
   }
 
@@ -144,7 +147,7 @@ const LeafletGraph = (props) => {
 
     return (
       <Marker
-        key={data[idColumn]}
+        attribution={data[idColumn]}
         position={position}
         icon={iconPerson}
         onClick={() => handleMarkerClick(data, onMarkClick)}
@@ -160,6 +163,7 @@ const LeafletGraph = (props) => {
   const handleSearch = (data, isSuccess) => {
     if (isSuccess && !isEqual(stateData, data)) {
       setStateData(data);
+      setLine([]);
     }
   }
 
@@ -175,6 +179,102 @@ const LeafletGraph = (props) => {
         handleSearch={handleSearch}
       />
     );
+  }
+
+  const handleClustererClick = (clusters) => {
+    const markers = [];
+
+    // get all markers from all clusters
+    clusters.layer.getAllChildMarkers().forEach(cluster => {
+      markers.push({
+        name: cluster.options.attribution,
+        position: { lat: cluster.options.position[0], lng: cluster.options.position[1] }
+      });
+    });
+
+    calculatePolylines(markers);
+  }
+
+  const calculatePolylines = (currentMarkers) => {
+
+    if (links && links.source && props[links.source].length) {
+
+      const polylines = [];
+      props[links.source].forEach(line => {
+
+        let destMarker = null;
+        let sourceMarker = null;
+
+        if (line[links.destinationColumn] && line[links.sourceColumn]) {
+          // check link source and destination id's in marker list to get marker's latlng
+          const dest = currentMarkers.find(marker => marker.name === line[links.destinationColumn]);
+          const source = currentMarkers.find(marker => marker.name === line[links.sourceColumn]);
+          if (!destMarker && dest) {
+            destMarker = dest.position;
+          }
+
+          if (!sourceMarker && source) {
+            sourceMarker = source.position;
+          }
+
+          // if any source or destintion point not found in bound, then find lat lng in data
+          if (!destMarker && sourceMarker) {
+            
+            const destination = stateData.find(d => d[idColumn] === line[links.destinationColumn])
+
+            if (destination) {
+              destMarker = {
+                lat: destination[latitudeColumn],
+                lng: destination[longitudeColumn],
+              }
+            }
+          }
+
+          // if any source or destintion point not found in bound, then find lat lng in data
+          if (!sourceMarker && destMarker) {
+            const source = stateData.find(d => d[idColumn] === line[links.sourceColumn]);
+
+            if (source) {
+              sourceMarker = {
+                lat: source[latitudeColumn],
+                lng: source[longitudeColumn],
+              }
+            }
+          }
+
+          if (destMarker && sourceMarker) {
+            if (!isPolylineExist(polylines, sourceMarker, destMarker)) {
+              polylines.push({
+                source: { lat: sourceMarker.lat, lng: sourceMarker.lng },
+                destination: { lat: destMarker.lat, lng: destMarker.lng },
+                color: line.color || theme.palette.redColor,
+              })
+            }
+          }
+        }
+
+      })
+      setLine(polylines);
+    }
+  }
+
+  // check line is already drawn or not
+  const isPolylineExist = (polylines, sourceMarker, destMarker) => {
+    return polylines.some(el => {
+      return ((el.source.lat === sourceMarker.lat && el.source.lng === sourceMarker.lng) && (el.destination.lat === destMarker.lat && el.destination.lng === destMarker.lng));
+    });
+  }
+
+  const renderPolylineIfNeeded = () => {
+    return line.map((link, i) => {
+      return <Polyline
+        color= {link.color}
+        positions={[
+          [link.source.lat, link.source.lng],
+          [link.destination.lat, link.destination.lng]
+        ]}
+      />
+    })
   }
 
   const createClusterCustomIcon = (clusters) => {
@@ -218,8 +318,10 @@ const LeafletGraph = (props) => {
           <MarkerClusterer
             iconCreateFunction={createClusterCustomIcon}
             showCoverageOnHover={false}
+            onClusterClick={handleClustererClick}
           >
             {renderMarkersIfNeeded()}
+            {renderPolylineIfNeeded()}
           </MarkerClusterer>
         </Map>
       }
