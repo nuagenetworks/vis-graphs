046db80b89e08279bd023f75af82689daaede029
Anitha <32629188+myTechPlayArea@users.noreply.github.com>
2022-01-07 11:06:08 -0800

    Monitoring graph not loading (#480)

--- Graphs/Table/index.js
+++ Graphs/Table/index.js
@@ -180,8 +180,8 @@ const TableGraph = (props) => {
         id,
     } = properties;
 
-    const vizID = id.replace(/-/g, '');
-    const contextKey = Object.keys(context).filter(key => key.startsWith(vizID) && key.endsWith('size'));
+    const vizID = id && id.replace(/-/g, '');
+    const contextKey = vizID && Object.keys(context).filter(key => key.startsWith(vizID) && key.endsWith('size'));
     const customSize = contextKey && context[contextKey] !== '1000' ? context[contextKey] : size;
 
     const getColumns = () => (properties.columns || []);
